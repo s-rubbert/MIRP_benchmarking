6c54f7cdb51ec96f946a367c8f2eff6c4a2fa8f0
Rubbert <sebastian.rubbert@cml.fraunhofer.de>
2023-01-11 10:36:52 +0100

    #25 updated code for local execution

--- run_optimizations_from_LP.py
+++ run_optimizations_from_LP.py
@@ -1,11 +1,17 @@
 import dimod
-import numpy as np
 import pickle
-from dwave.system import LeapHybridCQMSampler
+from dwave.cloud import Client
+# The token data is individual (and therefore non-public). 
+# If you have an import error here, you need to create your own 
+# dwave_token.py file and define the "value" variable as your 
+# dwave token.
+import dwave_token
+
+client = Client(token=dwave_token.value)
 
 
 file = 'LR1_1_DR1_3_VC1_V7a_t45.lp'
-# file = 'test.lp'
+#file = 'test.lp'
 
 model = dimod.lp.load('LP/' + file)
 print('loading done')
@@ -14,7 +20,7 @@ print('loading done')
 auxx_variables = [name for name in model.variables if '(auxx)' in name]
 for name in auxx_variables:
     model.fix_variable(name, 1)
-model.fix_variable('z_0,0', 0)
+model.fix_variable('z_0_0', 0)
 
 trivial_constraints = [label for label, constraint in model.constraints.items() if len(constraint.lhs.variables) == 0]
 for label in trivial_constraints:
@@ -22,14 +28,15 @@ for label in trivial_constraints:
 print('removed trivial constraints')
 print(f'remaining constraints: {len(model.constraints)}')
 
-sampler = LeapHybridCQMSampler()
-sample_set = sampler.sample_cqm(model, time_limit=5)
-
-print('calculation done')
+sampler = client.get_solver(name='hybrid_constrained_quadratic_model_version1p')
+sample_set = sampler.sample_cqm(model, time_limit=18000)
+sample_set = sample_set.sampleset
 
 sample_set = sample_set.to_serializable()
+print('calculation done')
 
 with open(f"Results/{file.split('.')[0]}" , 'wb') as f:
     pickle.dump(sample_set, f)
 
 print('all done')
+client.close()
